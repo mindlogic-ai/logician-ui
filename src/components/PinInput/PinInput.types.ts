e0acd61c61edccc33f435aec78dabc166c860a98
nk <namkyung.jo@mindlogic.ai>
2026-04-29 18:55:46 +0900

    ♻️ Lint

--- src/components/PinInput/PinInput.types.ts
+++ src/components/PinInput/PinInput.types.ts
@@ -1,17 +1,16 @@
 import type { InputHTMLAttributes } from 'react';
 import type { PinInput as ChakraPinInput } from '@chakra-ui/react';
 
-export interface PinInputProps
-  extends Omit<
-    ChakraPinInput.RootProps,
-    | 'count'
-    | 'value'
-    | 'defaultValue'
-    | 'onValueChange'
-    | 'onValueComplete'
-    | 'onChange'
-    | 'children'
-  > {
+export interface PinInputProps extends Omit<
+  ChakraPinInput.RootProps,
+  | 'count'
+  | 'value'
+  | 'defaultValue'
+  | 'onValueChange'
+  | 'onValueComplete'
+  | 'onChange'
+  | 'children'
+> {
   length: number;
   value?: string;
   onChange?: (value: string) => void;
